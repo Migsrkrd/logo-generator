f926192a2b6775876769259d53d4e55e021d635d
Mike <mreickcastillero@gmail.com>
2023-10-21 12:37:40 -0700

    added essential files and npms to the repo

---
+++ index.js
@@ -0,0 +1,10 @@
+// create file layout provided by instructions
+// add in inquirer for questions
+// look at svg resources in the readme for more info on the doc type
+// 
+// use inquirer to gather proper responses
+// use the response keys and apply them into shape functions
+//shape will be a class
+//each object will inherant shape properties
+//create a test for each shape
+
