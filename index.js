08c8108a038cd21f71306ed5d9b1bbdd6bb41b6b
Mike <mreickcastillero@gmail.com>
2023-10-21 12:57:37 -0700

    added logo generator js and added user prompt in index.js

--- index.js
+++ index.js
@@ -8,3 +8,35 @@
 //each object will inherant shape properties
 //create a test for each shape
 
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateSvg = require("./lib/LogoGenerator")
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Please enter three letters that you would like to include in your logo",
+            name: "Letters",
+        },
+        {
+            type:"input",
+            message: "What would you like the color of those letters to be?",
+            name: "TextColor",
+        },
+        {
+            type: "checkbox",
+            message: "What shape would you like your logo to be in?",
+            choices: ["Circle", "Triangle", "Square"],
+            name: "Shape",
+        },
+        {
+            type: "input",
+            message: "What color would you like your shape to be?",
+            name: "ShapeColor"
+        }
+    ])
+
+    .then((response)=>{
+        console.log(response)
+    })
